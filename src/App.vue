32f0116ac42429ee15d14b7f409aaf48c95ab010
Clement Procureur <clement.procureur@wearesocial.net>
2016-06-02 16:54:40 +0200

    data reactive cache

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,12 @@
       <h1>{{ progress }}%</h1>
     </div>
     <router-view></router-view>
+
+    <div class="" v-for="itm in img_cache">
+      {{ itm | json }}
+      <img v-bind:src="itm.src" alt="" />
+    </div>
+
   </div>
 </template>
 
@@ -13,49 +19,57 @@ export default {
   data () {
     return {
       progress: 0,
-      cache: {}
+      img_cache: [],
+      vid_cache: {}
     }
   },
-  events: {
-        'load:progress': 'onLoadProgress',
-        'load:complete': 'onLoadComplete'
-    },
 
     // Static manifest
     manifest: [
-      './static/img/test.jpg',
-      './static/video/bg.mp4',
-      './static/video/bg.webm',
-      './static/video/scroll.mp4',
-      './static/video/scroll.webm',
-      './static/video/test.webm',
-      './static/sound/sanik.mp3',
+      './static/img/test.jpg'
+      // './static/video/bg.mp4',
+      // './static/video/bg.webm',
+      // './static/video/scroll.mp4',
+      // './static/video/scroll.webm',
+      // './static/video/test.webm',
+      // './static/sound/sanik.mp3',
     ],
 
     ready: function() {
-        let Loader = require('resource-loader');
-        let loader = new Loader();
+      let Loader = require('resource-loader');
+      let loader = new Loader();
 
-        let manifest = this.$options.manifest;
+      let that = this;
+      let manifest = that.$options.manifest;
 
-        manifest.forEach(function(file) {
-            loader.add(file, file);
-        });
-        // loader.add('./static/img/test.jpg', './static/img/test.jpg');
+      manifest.forEach(function(file) {
+          loader.add(file, file);
+      });
 
-        let that = this;
-        loader.on('progress', function(event, other){
-          that.progress = Math.round(event.progress);
-          console.log('progress', this.progress);
-        });
+      loader.on('progress', function(event, resource){
+        that.progress = Math.round(event.progress);
+        console.log('progress', this.progress);
+        // console.log(resource);
+        if(resource.url.match(/\.(jpe?g|png|gif|bmp)$/i)){
+          console.log('IMG');
+          that.img_cache.push({
+            'name': resource.name,
+            'src': resource.url
+          });
+        }else {
+          console.log('other...');
+          that.vid_cache[resource.name] = {'src': resource.url}
+        }
+      });
 
-        loader.on('complete', function(event, resources){
-          console.log('COMPLETE');
-          that.cache = resources;
-          that.$route.router.go('/intro');
-        });
+      loader.on('complete', function(event, resources){
+        console.log('COMPLETE');
+        console.log(that.img_cache);
+        // that.img_cache = resources;
+        // that.$route.router.go('/intro');
+      });
 
-        loader.load();
+      loader.load();
     }
 }
 </script>
