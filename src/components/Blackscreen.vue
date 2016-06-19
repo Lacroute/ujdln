0b2f9928849ef638353044b4d60bb076dc7c3596
lacroute <mc.flurry@hotmail.fr>
2016-06-19 20:53:14 +0200

    blackscreen

---
+++ src/components/Blackscreen.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="blackscreen">
+    <p>
+      {{ blackscreen.text }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Blackscreen',
+
+  props: [
+    'blackscreen'
+  ],
+
+  ready: function(){
+    let that = this
+    let delay = this.blackscreen.text.split(' ').length * 450
+
+    // remove the blackscreen after a certain amount of time
+    setTimeout(function(){
+      // that.$dispatch('end-blackscreen', that.blackscreen)
+    }, delay)
+  }
+}
+</script>
+
+<style>
+.blackscreen {
+  background: black;
+  color: white;
+
+}
+</style>
--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,13 +1,36 @@
 <template>
-  <h1>INTRO</h1>
-  <a v-link="{ path: '/home' }">Continue to Home</a>
+  <div v-if="begin">
+    <blackscreen v-for="blackscreen in bs" :blackscreen="blackscreen" @end-blackscreen="removeBlackscreen">
+  </div>
+  <div v-else>
+    <h1>INTRO</h1>
+    <a v-link="{ path: '/home' }">Continue to Home</a>
+  </div>
 </template>
 
 <script>
-import Loader from './Loader'
+import Blackscreen from './Blackscreen.vue'
 
 export default {
   name: 'Intro',
-  components: {Loader}
+  components: {Blackscreen},
+
+  data () {
+    return {
+      begin: true,
+      bs: []
+    }
+  },
+
+  ready: function(){
+    this.bs.push({text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'});
+  },
+
+  methods: {
+    removeBlackscreen: function(blackscreen){
+      this.bs.$remove(blackscreen);
+      this.begin = false;
+    }
+  }
 }
 </script>
